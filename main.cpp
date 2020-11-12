b8b6fc00f91aa689c438863c49795f1eb32b5b2f
Jiajie Chen <c@jia.je>
2020-11-12 12:16:57 +0800

    Add bandwidth test

--- main.cpp
+++ main.cpp
@@ -44,10 +44,15 @@ int main(int argc, char *argv[])
         }
     }
 
-    // latency test
+    // buffer
     const int N = 1024;
-    const int loop_times = 10240;
-    const int skip_times = 2048;
+    const int M = 65536;
+    char send_buffer[M] = {0};
+    char recv_buffer[M] = {0};
+
+    // latency test
+    const int loop_times = 4096;
+    const int skip_times = 1024;
 
     double *latency = nullptr;
     if (my_id == 0)
@@ -69,8 +74,6 @@ int main(int argc, char *argv[])
         }
     }
 
-    char send_buffer[N] = {0};
-    char recv_buffer[N] = {0};
     for (auto [from, to] : comms)
     {
         double elapsed = 0.0;
@@ -86,6 +89,7 @@ int main(int argc, char *argv[])
                 {
                     time_start = MPI_Wtime();
                 }
+                // 2 rtt
                 MPI_Send(send_buffer, N, MPI_CHAR, to, 0, MPI_COMM_WORLD);
                 MPI_Recv(recv_buffer, N, MPI_CHAR, to, 0, MPI_COMM_WORLD, &status);
             }
@@ -111,8 +115,9 @@ int main(int argc, char *argv[])
         if (my_id == 0)
         {
             double us = actual_elapsed * 1000 * 1000 / 2 / loop_times;
-            // printf("%d <-> %d: %.2fus\n", from, to, us);
+            printf("%d <-> %d: %.2fus\n", from, to, us);
             latency[from * num_procs + to] = us;
+            fflush(stdout);
         }
     }
 
@@ -150,12 +155,78 @@ int main(int argc, char *argv[])
                 printf("Found anomaly: %d <-> %d %.2fus\n", from, to, data);
             }
         }
+        fflush(stdout);
+    }
+
+    // bandwidth test
+    double *bandwidth = nullptr;
+    const int batch_size = 64;
+    MPI_Request request[batch_size];
+    MPI_Status status[batch_size];
+    if (my_id == 0)
+    {
+        bandwidth = (double *)malloc(sizeof(double) * num_procs * num_procs);
+        assert(bandwidth != nullptr);
+    }
+    for (auto [from, to] : comms)
+    {
+        double elapsed = 0.0;
+        MPI_Barrier(MPI_COMM_WORLD);
+        double time_start = 0.0;
+
+        if (my_id == from)
+        {
+            for (int i = 0; i < loop_times + skip_times; i++)
+            {
+                if (i == skip_times)
+                {
+                    time_start = MPI_Wtime();
+                }
+
+                for (int j = 0; j < batch_size; j++)
+                {
+                    MPI_Isend(send_buffer, M, MPI_CHAR, to, 0, MPI_COMM_WORLD, &request[j]);
+                }
+                MPI_Waitall(batch_size, request, status);
+                MPI_Recv(recv_buffer, 1, MPI_CHAR, to, 1, MPI_COMM_WORLD, &status[0]);
+            }
+        }
+        else if (my_id == to)
+        {
+            for (int i = 0; i < loop_times + skip_times; i++)
+            {
+                for (int j = 0; j < batch_size; j++)
+                {
+                    MPI_Irecv(recv_buffer, M, MPI_CHAR, from, 0, MPI_COMM_WORLD, &request[j]);
+                }
+                MPI_Waitall(batch_size, request, status);
+                MPI_Send(send_buffer, 1, MPI_CHAR, from, 1, MPI_COMM_WORLD);
+            }
+        }
+
+        double time_end = MPI_Wtime();
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (my_id == from)
+        {
+            elapsed = time_end - time_start;
+        }
+
+        double actual_elapsed = 0.0;
+        MPI_Reduce(&elapsed, &actual_elapsed, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        if (my_id == 0)
+        {
+            double mb = (double)M * batch_size * loop_times / actual_elapsed / 1000 / 1000 / 1000 * 8;
+            printf("%d <-> %d: %.2fGbps/s\n", from, to, mb);
+            bandwidth[from * num_procs + to] = mb;
+            fflush(stdout);
+        }
     }
 
     if (my_id == 0)
     {
         free(all_hostnames);
         free(latency);
+        free(bandwidth);
     }
 
     MPI_Finalize();
